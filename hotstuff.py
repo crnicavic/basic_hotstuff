52c85d7b8aba89faa4eef573ec8824542050f624
crnicavic <cavic5000@tutanota.com>
2026-01-09 20:21:29 +0100

    leader proposes a block! minor bugfixes

--- hotstuff.py
+++ hotstuff.py
@@ -1,9 +1,13 @@
-from concurrent.futures import ThreadPoolExecutor
+from enum import Enum
 
 N = 4
 F = 1
-QUORUM = N - F
+QUORUM = 2*F+1
 NEW_VIEW = 5421315
+
+class Message_types:
+	NEW_VIEW = 5421315
+	PREPARE = 5421316
 
 class Block:
 	def __init__(self, cmds, parent, view):
@@ -11,10 +15,30 @@ class Block:
 		self.parent = parent
 		self.height = view;
 	
-	def __str__():
+	def __str__(self):
 		return f"Block(height:{self.height})"
 
-ZERO_BLOCK = Block([], None, 0)
+GENESIS_BLOCK = Block([], None, 0)
+
+class QC:
+	def __init__(self, qc_type, view_number, block):
+		self.qc_type = qc_type
+		self.view_number = view_number
+		self.block = block
+		self.voters = []
+
+	def __str__(self):
+		ret = f"QC(type:{self.qc_type};"
+		ret += f"view_number:{self.view_number};"
+		ret += f"block:{self.block};"
+		ret += f"voters:{self.voters};)"
+		return ret
+
+GENESIS_QC = QC(
+		Message_types.PREPARE,
+		0,
+		GENESIS_BLOCK
+)
 
 class Message:
 	def __init__(self, msg_type, view_number, block, qc):
@@ -30,54 +54,52 @@ class Message:
 		ret += f"justify_qc:{self.justify};)"
 		return ret
 
-class QC:
-	def __init__(self, qc_type, view_number, node):
-		self.qc_type = qc_type
-		self.view_number = view_number
-		self.node = node
-		self.voters = []
-
-	def __str__():
-		ret = f"QC(type:{self.qc_type};"
-		ret += f"view_number:{self.view_number};"
-		ret += f"block:{self.node};"
-		ret += f"voters:{self.voters};)"
-		return ret
-
 class Replica:
-	log = []
-
 	def __init__(self, replica_id):
 		self.replica_id = replica_id
 		self.current_view = 0
-		self.log.append(ZERO_BLOCK)
+		self.log = [GENESIS_BLOCK]
 		self.new_view_msgs = {}
-		self.high_prepare_qc = None
+		self.high_prepare_qc = GENESIS_QC
 
 	def on_client_request(self, replicas):
 		print("received request from client")
-		self.current_view += 1
-		leader_id = self.current_view % N
+		leader_id = (self.current_view + 1)% N
 		new_view_msg = Message(NEW_VIEW, self.current_view, None, self.high_prepare_qc)	
-		if leader_id == self.replica_id:
-			# leaders sends message to themselves
-			if new_view_msg.view_number not in self.new_view_msgs:
-				self.new_view_msgs[new_view_msg.view_number] = [new_view_msg]
-			elif len(self.new_view_msgs[new_view_msg.view_number]) <= QUORUM:
-				self.new_view_msgs[new_view_msg.view_number].append(new_view_msg)
-			return
-		
-		replicas[leader_id].on_new_view(new_view_msg)
+		self.current_view += 1
+		replicas[leader_id].on_new_view(new_view_msg, replicas)
 		return
 
-	def on_new_view(self, msg):
+	def on_new_view(self, msg, replicas):
 		if msg.view_number not in self.new_view_msgs:
 			self.new_view_msgs[msg.view_number] = [msg]
-		elif len(self.new_view_msgs[msg.view_number]) <= QUORUM:
-			self.new_view_msgs[msg.view_number].append(msg)
 		else:
-			self.new_view_msgs[msg.view_number].sort(key=lambda qc: qc.view_number)
+			self.new_view_msgs[msg.view_number].append(msg)
+
+		if len(self.new_view_msgs[msg.view_number]) == QUORUM:
+			sort_lambda = lambda m: m.justify.view_number 
+			self.new_view_msgs[msg.view_number].sort(key=sort_lambda)
+			self.high_prepare_qc = self.new_view_msgs[msg.view_number][-1].justify
+			proposal_block = Block(
+				f"cmd_{self.current_view}",
+				self.high_prepare_qc.block,
+				self.current_view
+			)
+			proposal_msg = Message(
+				Message_types.PREPARE,
+				self.current_view,
+				proposal_block,
+				self.high_prepare_qc
+			)
+			for r in replicas:
+				if r.replica_id != self.replica_id:
+					# also send who the message is from, so the replica knows
+					# where to send the vote
+					r.examine_prepare_proposal(proposal_msg, replicas[self.replica_id])
 		return
+
+	def examine_prepare_proposal(self, message, leader):
+		print(f"received proposal for {message.block} from {leader}")
 
 	def __str__(self):
 		ret = f"replica_id:{self.replica_id} on view:{self.current_view}"
